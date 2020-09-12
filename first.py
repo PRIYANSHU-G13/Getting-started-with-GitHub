7c8c6c18a11fd82d8235269550f071005c21a625
Kathyayani-Bolgam <70484748+Kathyayani-Bolgam@users.noreply.github.com>
2020-09-12 14:37:31 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,3 +1,3 @@
 print('hello world')
 print("Let's get started with a simple python file which i created using a new branch named simple python.\n After creating it and writing some codes i'll raise a pull request which will help to insert my code in the main code")
-
+print("let's start")
